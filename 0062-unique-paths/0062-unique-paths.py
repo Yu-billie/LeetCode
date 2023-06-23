aecd47e0528d1ca3d1322246dac8ff759038d66d
Yumin Kim <110521952+Yu-billie@users.noreply.github.com>
2023-06-23 19:48:18 +0900

    Time: 44 ms (73.18%), Space: 16.4 MB (42.61%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,21 @@
+import math
+class Solution:
+    def uniquePaths(self, m, n): 
+        dp = [[0] * n for _ in range(m)]
+
+        # Initialize the first row and first column
+        for i in range(m):
+            dp[i][0] = 1
+        for j in range(n):
+            dp[0][j] = 1
+
+        # Calculate the number of unique paths for each cell
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[m - 1][n - 1]
+
+
+s = Solution()
+print(s.uniquePaths(3,2))
